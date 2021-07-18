f32112939f611a345e94291276e9d7b59b3277ec
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-07-17 21:34:05 -0400

    imported nav bar from react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import About from './pages/AboutMe';
+import { Navbar } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
 
 
 
 function App() {
   return (
     <div className="App">
-<h1>Hello</h1>
+<Navbar bg="dark" variant="dark">
+<Container>
+<Navbar.Brand href="#home">Navbar</Navbar.Brand>
+<Nav className="me-auto">
+  <Nav.Link href="#home">Home</Nav.Link>
+  <Nav.Link href="#features">Features</Nav.Link>
+  <Nav.Link href="#pricing">Pricing</Nav.Link>
+</Nav>
+</Container>
+</Navbar>
 <About/>
     </div>
   );
