9de92743ba2607cf2995d24f45edf7ae7336f839
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-07-17 22:11:37 -0400

    styled components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import About from './pages/AboutMe';
 import { Navbar } from 'react-bootstrap';
 import { Container } from 'react-bootstrap';
 import { Nav } from 'react-bootstrap';
+import './styling/App.css'
 
 
 
@@ -11,11 +12,11 @@ function App() {
     <div className="App">
 <Navbar bg="dark" variant="dark">
 <Container>
-<Navbar.Brand href="#home">Navbar</Navbar.Brand>
+<Navbar.Brand href="#home">Jasper Abarquez</Navbar.Brand>
 <Nav className="me-auto">
   <Nav.Link href="#home">Home</Nav.Link>
-  <Nav.Link href="#features">Features</Nav.Link>
-  <Nav.Link href="#pricing">Pricing</Nav.Link>
+  <Nav.Link href="#features">Projects</Nav.Link>
+  <Nav.Link href="#pricing">Contact</Nav.Link>
 </Nav>
 </Container>
 </Navbar>
--- src/components/AboutComponents/AboutSection.js
+++ src/components/AboutComponents/AboutSection.js
@@ -1,29 +1,54 @@
 import React from 'react'
 import Selfie from '../../Assets/HawaiianSelfie.jpg'
+import styled from 'styled-components'
+
+
+const AboutMeStyle = styled.div `
+background: blue;
+display: flex;
+align-items: center;
+justify-content: center;
+height:50vh;
+padding-left: 30%;
+padding-right: 30%;
+
+.description{
+    background:red;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 600px;
+}
+
+.HawaiianSelfie{
+height: 70%;
+width: 70%;
+margin-top: 1rem;
+margin-left: 20%;
+
+}
+
+button{
+    width: 50%;
+}
+
+`
 
 const AboutSection = () => {
 
     return (
-
         <div>
+            <AboutMeStyle>
             <div className="description">
-                <div className="title">
-                    <div className="hide">
-                        <h2>I work to make </h2>
-                    </div>
-                    <div className="hide">
-                        <h2>your <span>dreams</span> come  </h2>
-                    </div>
-                    <div className="hide">
-                        <h2>true. </h2>
-                    </div>
-                </div>
+                <h2>About Me</h2>
                 <p>Contact us for any ideas</p>
-                <button>Conact me</button>
+                <button>View My Resume</button>
             </div>
             <div className="image">
-                <img src={Selfie} alt="Pictue of Jasper in a Hawaiian shirt" />
+                <img className="HawaiianSelfie" src={Selfie} alt="Pictue of Jasper in a Hawaiian shirt" />
             </div>
+            </AboutMeStyle>
         </div>
 
     )
