19ac2d976b5a0484c2583ab1b73470ed42ea2e3b
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-07-22 12:47:40 -0400

    set up links in nav bar with react router

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Nav = ()=> {
     return (
         <div>
             <h1>Nav Bar</h1>
+            <div className= "links">
+               <ul>
+                   <li>
+                       <Link to="/">About Me</Link>
+                   </li>
+                   <li>
+                       <Link to="/projects">Projects</Link>
+                   </li>
+                   <li>
+                       <Link to="/contact">Contact</Link>
+                   </li>
+               </ul>
+            </div>
         </div>
     )
 }
