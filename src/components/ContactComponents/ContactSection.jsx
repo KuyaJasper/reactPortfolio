671ff365e96d1c5b945134bd518fe65a0ab1ed62
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-07-22 15:29:35 -0400

    set up stylign for contact added icons to footer

--- src/components/ContactComponents/ContactSection.jsx
+++ src/components/ContactComponents/ContactSection.jsx
@@ -1,60 +1,180 @@
-import React from 'react'
-import styled from 'styled-components';
-import Sunrise from '../../Assets/LanternSunrise.jpg'
+import React from "react";
+import styled from "styled-components";
+import Sunrise from "../../Assets/LanternSunrise.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faUser } from "@fortawesome/free-regular-svg-icons";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import {
+  faFile,
+  faGlobeAmericas,
+  faMobileAlt,
+} from "@fortawesome/free-solid-svg-icons";
 
 const ContactSection = () => {
-    return (
-        <ContactStyle>
-            <div className="titleContainer">
-                <h1 className="title" >Contact Info</h1>
-            </div>
-        <div className="projectContainer" >
-            <h1>Projects Go Here</h1>
+  return (
+    <ContactStyle>
+      <div className="titleContainer">
+        <h1 className="title">Contact Info</h1>
+      </div>
+      <div className="contactContainer">
+        <h1>Let's Connect!</h1>
+        <h4>You Can Reach Me At:</h4>
+        <div className="contactInfo">
+          {/* First Column */}
+          <ul className="social">
+            <li>
+              <FontAwesomeIcon icon={faUser} size="3x" color="whitesmoke" />
+              <span>Jasper Abarquez</span>
+            </li>
+            <li>
+              <FontAwesomeIcon
+                icon={faMobileAlt}
+                size="3x"
+                color="whitesmoke"
+              />
+              <span>(908)-472-2099</span>
+            </li>
+            <li>
+              <a
+                href="mailto:abarquezj1@gmail.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon
+                  icon={faEnvelope}
+                  size="3x"
+                  color="whitesmoke"
+                />
+                {' '}
+                abarquezj1@gmail.com
+              </a>
+            </li>
+            <li>
+              <FontAwesomeIcon
+                icon={faGlobeAmericas}
+                size="3x"
+                color="whitesmoke"
+              />
+              <span>Edison, New Jersey</span>
+            </li>
+
+            {/* SecondColumn */}
+          </ul>
+          <ul className="social2">
+            <li>
+              <FontAwesomeIcon icon={faUser} size="3x" color="whitesmoke" />
+              <span>Jasper Abarquez</span>
+            </li>
+            <li>
+              <a
+                className="linkedin"
+                href="https://www.linkedin.com/in/jasper-abarquez/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon icon={faLinkedin} size="3x" color="blue" />
+              </a>
+              <span>LinkedIn</span>
+            </li>
+            <li>
+              <a
+                className="github"
+                href="https://github.com/KuyaJasper"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon
+                  icon={faGithub}
+                  size="3x"
+                  color="rgb(220,20,60)"
+                />
+              </a>
+              <span>KuyaJasper</span>
+            </li>
+            <li>
+              <a
+                className="github"
+                href="https://github.com/KuyaJasper"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon icon={faFile} size="3x" color="whitesmoke" />
+              </a>
+              <span>View Resume</span>
+            </li>
+          </ul>
         </div>
-        </ContactStyle>
-    )
-  };
-  export default ContactSection;
+      </div>
+    </ContactStyle>
+  );
+};
+export default ContactSection;
 
+const ContactStyle = styled.div`
+  color: whitesmoke;
 
-
-
-  const ContactStyle = styled.div `
-  .titleContainer{
+  .linkStyle {
+    padding-left: 1rem;
+  }
+  a {
+    text-decoration: none;
+    color: whitesmoke;
+  }
+  span {
+    padding: 1rem;
+  }
+  ul {
+    list-style: none;
+  }
+  li {
+    padding-top: 1rem;
+  }
+  .titleContainer {
     background-image: url(${Sunrise});
     height: 100vh;
     background-position: center;
-background-repeat: no-repeat;
-background-size: cover;
-background-attachment: fixed;
-max-height: 100%;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-attachment: fixed;
+    max-height: 100%;
   }
-  .title{
+  .title {
     display: flex;
     justify-content: flex-end;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  font-size: 150px;
-  padding-right: 1rem;
-  color: whitesmoke;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    font-size: 150px;
+    padding-right: 1rem;
+    color: whitesmoke;
 
-  /* 3d effect */
-  top: 35%;
-  width: 100%;
+    /* 3d effect */
+    top: 35%;
+    width: 100%;
 
-  text-shadow: 0 1px 0 hsl(174, 5%, 80%), 0 2px 0 hsl(174, 5%, 75%),
-    0 3px 0 hsl(174, 5%, 70%), 0 4px 0 hsl(174, 5%, 66%),
-    0 5px 0 hsl(174, 5%, 64%), 0 6px 0 hsl(174, 5%, 62%),
-    0 7px 0 hsl(174, 5%, 61%), 0 8px 0 hsl(174, 5%, 60%),
-    0 0 5px rgba(0, 0, 0, 0.05), 0 1px 3px rgba(0, 0, 0, 0.2),
-    0 3px 5px rgba(0, 0, 0, 0.2), 0 5px 10px rgba(0, 0, 0, 0.2),
-    0 10px 10px rgba(0, 0, 0, 0.2), 0 20px 20px rgba(0, 0, 0, 0.3);}
+    text-shadow: 0 1px 0 hsl(174, 5%, 80%), 0 2px 0 hsl(174, 5%, 75%),
+      0 3px 0 hsl(174, 5%, 70%), 0 4px 0 hsl(174, 5%, 66%),
+      0 5px 0 hsl(174, 5%, 64%), 0 6px 0 hsl(174, 5%, 62%),
+      0 7px 0 hsl(174, 5%, 61%), 0 8px 0 hsl(174, 5%, 60%),
+      0 0 5px rgba(0, 0, 0, 0.05), 0 1px 3px rgba(0, 0, 0, 0.2),
+      0 3px 5px rgba(0, 0, 0, 0.2), 0 5px 10px rgba(0, 0, 0, 0.2),
+      0 10px 10px rgba(0, 0, 0, 0.2), 0 20px 20px rgba(0, 0, 0, 0.3);
+  }
 
-    .projectContainer{
-        display: flex;
-        height: 100vh;
-        background: black;
-    }
-
-
-  
-  `
+  .contactContainer {
+    height: 100vh;
+    display: flex;
+    background: black;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+  }
+  .contactInfo {
+    background-color: RGBA(23, 23, 23);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 1rem;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+  }
+`;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,107 +1,123 @@
-import React from 'react'
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 //ICONS
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faLinkedin,
-  faGithub,
-} from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faMobileAlt } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
-const Footer = ()=> {
-    return (
-        <FooterStyle>
-            <div className="navigation">
-                <h4>Nav Links</h4>
-                <ul>
-                <li>
-                       <Link to="/">About Me</Link>
-                   </li>
-                   <li>
-                       <Link to="/projects">Projects</Link>
-                   </li>
-                   <li>
-                       <Link to="/contact">Contact</Link>
-                   </li>
-                </ul>
-            </div>
-            <div className="contact">
-                <h4>Contact</h4>
-                <ul>
-                    <li>Phone: (908)472-2099</li>
-                    <li>Email: abarquezj1@gmail.com</li>
-                </ul>
-
-            </div>
-            <div className="socialContainer">
-                <ul className="social">
-                <li>
-                <a
-                  className="linkedin"
-                  href="https://www.linkedin.com/in/jasper-abarquez/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FontAwesomeIcon icon={faLinkedin} size="3x" color="blue" />
-                </a>
-              </li>
-              <li>
-                <a
-                  className="github"
-                  href="https://github.com/KuyaJasper"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FontAwesomeIcon icon={faGithub} size="3x" color="rgb(220,20,60)" />
-                </a>
-              </li>
-                </ul>
-            </div>
-            </FooterStyle>
-
-    )
-}
+const Footer = () => {
+  return (
+    <FooterStyle>
+      <div className="navigation">
+        <h4>Nav Links</h4>
+        <ul>
+          <li>
+            <Link to="/">About Me</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="contact">
+        <h4>Contact</h4>
+        <ul>
+          <li>
+            <FontAwesomeIcon icon={faMobileAlt} size="2x" color="whitesmoke" />{" "}
+            (908)472-2099
+          </li>
+          <li>
+            <a
+              href="mailto:abarquezj1@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon icon={faEnvelope} size="2x" color="whitesmoke" />{" "}
+              abarquezj1@gmail.com
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="socialContainer">
+        <ul className="social">
+          <li>
+            <a
+              className="linkedin"
+              href="https://www.linkedin.com/in/jasper-abarquez/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon icon={faLinkedin} size="3x" color="blue" />
+            </a>
+          </li>
+          <li>
+            <a
+              className="github"
+              href="https://github.com/KuyaJasper"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faGithub}
+                size="3x"
+                color="rgb(220,20,60)"
+              />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </FooterStyle>
+  );
+};
 
 export default Footer;
 
-
 const FooterStyle = styled.div`
-background-color:RGBA(23, 23, 23); 
-display: flex;
-justify-content: space-space-around;
-color: whitesmoke;
-ul{
+  background-color: RGBA(23, 23, 23);
+  display: flex;
+  justify-content: space-space-around;
+  color: whitesmoke;
+
+  h4 {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  }
+  ul {
     display: flex;
     flex-direction: column;
     list-style: none;
-}
-li{
+  }
+  li {
     padding-left: 1rem;
-    padding-right: 1rem;   
-    padding-top : 1rem;
-}
-a{
+    padding-right: 1rem;
+    padding-top: 1rem;
+  }
+  a {
     text-decoration: none;
     color: whitesmoke;
-}
+  }
 
-.navigation{
+  .navigation {
     margin-left: 1rem;
     display: flex;
     align-items: center;
-}
+  }
 
-.contact{
+  .contact {
     margin-left: 1rem;
     display: flex;
     align-items: center;
-}
-.social{
+  }
+  .social {
     margin-top: 1rem;
     display: flex;
     align-items: center;
     flex-direction: row;
-}
-
-`
+  }
+`;
