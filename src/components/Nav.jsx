dd7c87fa8683d7fee90a0ed48bf348c3d0fc6548
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-07-22 13:16:23 -0400

    set up nav bar with links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 const Nav = ()=> {
     return (
-        <div>
-            <h1>Nav Bar</h1>
+        <NavStyle>
+        <div className="Name">
+        <h1>Jasper Abarquez</h1>
             <div className= "links">
                <ul>
                    <li>
@@ -19,7 +21,38 @@ const Nav = ()=> {
                </ul>
             </div>
         </div>
+        </NavStyle>
     )
 }
 
 export default Nav;
+
+
+const NavStyle = styled.div `
+background:grey;
+z-index: 2;
+ul{
+    display: flex;
+    list-style: none;
+}
+li{
+    padding-left: 1rem;
+    padding-right: 1rem;   
+    padding-top : 1rem;
+}
+a{
+    text-decoration: none;
+    color: white;
+}
+.links{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    
+    
+}
+.Name{
+    display: flex;
+    justify-content: flex-start;
+}
+`
